2dedaadde6a330765c0bcc0ea1b35362188d08ab
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 19:51:01 +0200

    Updated description of package.

--- Formula/timer-for-python.rb
+++ Formula/timer-for-python.rb
@@ -1,7 +1,7 @@
 class TimerForPython < Formula
   include Language::Python::Virtualenv
 
-  desc "Makes it easy to measure time and performance of Python programs"
+  desc "Makes it easy to submit URLs to the IndexNow API of various search engines"
   homepage "https://github.com/jakob-bagterp/index-now-for-python"
   url "https://github.com/jakob-bagterp/index-now-for-python/releases/download/v0.0.1/index_now_for_python-0.0.1.tar.gz"
   sha256 "8c1220677edd60c84d5c6af80fd08f3e302307d6311a7e4b9def8f8f124f8c14"
