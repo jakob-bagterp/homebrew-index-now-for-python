c520c392a2d15ac923c102b81235fea54bf5b7cc
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 19:47:42 +0200

    Updated temporary formula.

---
+++ Formula/timer-for-python.rb
@@ -0,0 +1,19 @@
+class TimerForPython < Formula
+  include Language::Python::Virtualenv
+
+  desc "Makes it easy to measure time and performance of Python programs"
+  homepage "https://github.com/jakob-bagterp/index-now-for-python"
+  url "https://github.com/jakob-bagterp/index-now-for-python/releases/download/v0.0.1/index_now_for_python-0.0.1.tar.gz"
+  sha256 "8c1220677edd60c84d5c6af80fd08f3e302307d6311a7e4b9def8f8f124f8c14"
+  license "MIT"
+
+  depends_on "python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "true"
+  end
+end
