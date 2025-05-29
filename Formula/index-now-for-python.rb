5cb1acc33e3967176e6ebaf5945676030dee7675
Jakob Bagterp <25110864+jakob-bagterp@users.noreply.github.com>
2025-05-29 16:25:15 +0200

    Updated formula to version 1.0.0

--- Formula/index-now-for-python.rb
+++ Formula/index-now-for-python.rb
@@ -3,8 +3,8 @@ class IndexNowForPython < Formula
 
   desc "Makes it easy to submit URLs to the IndexNow API of various search engines"
   homepage "https://github.com/jakob-bagterp/index-now-for-python"
-  url "https://github.com/jakob-bagterp/index-now-for-python/releases/download/v0.1.6/index_now_for_python-0.1.6.tar.gz"
-  sha256 "030631985278c3eeaf0a39cf535834f55c98e90252385162e200f5d4a260493b"
+  url "https://github.com/jakob-bagterp/index-now-for-python/releases/download/v1.0.0/index_now_for_python-1.0.0.tar.gz"
+  sha256 "8bbc994460aebc701cc278c7babd2ca6bc8e9cfee31b9b06f92146889b0cb6e2"
   license "MIT"
 
   depends_on "python@3.10"
